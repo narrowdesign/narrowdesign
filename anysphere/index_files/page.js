9273cb0e3c9ed57a6f9996fb2060a5b8c1cbf261
narrowdesign <nick@narrowdesign.com>
2024-04-25 09:49:33 -0400

    scroll

--- anysphere/index_files/page.js
+++ anysphere/index_files/page.js
@@ -1,28 +1,24 @@
-
 class PageScript {
   constructor() {
     this.winH = window.innerHeight;
-    // clientside javascript here
-    if (!window.location.search.includes('instrument')) {
-      document.body.classList.remove('isInitialized');
-    }
-    this.scrollActivatedEls = document.querySelectorAll('.jsScrollActivated');
-    if (this.scrollActivatedEls.length > 0) {
-      this.scrollActivatedTops = this.scrollActivatedEls.map((el) => {
-        el.getBoundingClientRect().top;
-      })
-      document.addEventListener('scroll', this.handleScroll);
-    }
+    this.scrollY = 0;
+    document.addEventListener("wheel", this.handleScroll);
   }
 
-  handleScroll = () => {
-    const scrollY = window.scrollY;
-    this.scrollActivatedTops.forEach((top, i) => {
-      if (scrollY > top + this.winH / 2) {
-        this.scrollActivatedEls[i].classList.add('isActive');
-      }
-    })
-  }
+  handleScroll = (e) => {
+    this.scrollY += e.deltaY;
+    // get the --background-color css variable value from the body element
+    const bgColor = window
+      .getComputedStyle(document.body)
+      .getPropertyValue("--background-color");
+    // cycle through the full color spectrum as the user scrolls
+    const hue = ((234 + this.scrollY * 0.05) % 360) + 1;
+    const sat = 19 + Math.sin(this.scrollY * 0.0004) * 19;
+    // create a radial gradient background color based on the hue so that the color changes smoothly
+
+    // set the new background color
+    document.body.style.backgroundColor = `hsl(${hue}, ${sat}%, 19%)`;
+  };
 }
 
 const pageScript = new PageScript();
