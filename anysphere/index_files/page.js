b14f7d573adb1cb9c26a81bba9727c3b499493c1
narrowdesign <nick@narrowdesign.com>
2024-05-14 19:55:35 -0400

    anysphere

--- anysphere/index_files/page.js
+++ anysphere/index_files/page.js
@@ -2,10 +2,69 @@ class PageScript {
   constructor() {
     this.winH = window.innerHeight;
     this.scrollY = 0;
-    document.addEventListener("wheel", this.handleScroll);
+
+    this.titleMenuItems = document.querySelectorAll(".titleMenu__title");
+    document.addEventListener("wheel", this.handleWheel);
+    document.addEventListener("scroll", this.handleScroll);
+    setTops();
+    window.addEventListener("resize", () => {
+      setTops();
+    });
+
+    function setTops() {
+      this.careersTop =
+        document.querySelector(".careers__copy").offsetTop -
+        window.innerHeight * 0.4 +
+        146;
+      this.blogTop =
+        document.querySelector(".blog__copy").offsetTop -
+        window.innerHeight * 0.4 +
+        146;
+    }
+
+    this.titleMenuItems[0].addEventListener("click", () => {
+      // smooth scroll to 0, 0
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    });
+    this.titleMenuItems[1].addEventListener("click", () => {
+      window.scrollTo({
+        top: this.careersTop + 4,
+        left: 0,
+        behavior: "smooth",
+      });
+    });
+    this.titleMenuItems[2].addEventListener("click", () => {
+      window.scrollTo({
+        top: this.blogTop + 4,
+        left: 0,
+        behavior: "smooth",
+      });
+    });
   }
 
   handleScroll = (e) => {
+    if (window.scrollY > this.careersTop - 150) {
+      this.titleMenuItems[0].classList.remove("isActive");
+      this.titleMenuItems[1].classList.add("isActive");
+      this.titleMenuItems[2].classList.remove("isActive");
+    } else {
+      this.titleMenuItems[0].classList.add("isActive");
+      this.titleMenuItems[1].classList.remove("isActive");
+      this.titleMenuItems[2].classList.remove("isActive");
+      return;
+    }
+    if (window.scrollY > this.blogTop - 150) {
+      this.titleMenuItems[0].classList.remove("isActive");
+      this.titleMenuItems[1].classList.remove("isActive");
+      this.titleMenuItems[2].classList.add("isActive");
+    }
+  };
+
+  handleWheel = (e) => {
     this.scrollY += e.deltaY;
     // get the --background-color css variable value from the body element
     const bgColor = window
